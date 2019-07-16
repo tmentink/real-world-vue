d852a8394065fa788919f17e1adeb3b89b8ee249
Trent Mentink <trentmentink@gmail.com>
2019-07-16 07:59:01 -0700

    completed global components

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -3,6 +3,7 @@
     <div class="event-card -shadow">
       <span class="eybrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
+      <BaseIcon name="users"/>
       <span>{{ event.attendees.length }} attending</span>
     </div>
   </router-link>
